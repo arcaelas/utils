6cc8329ba923632cf3746d41141265b86872518e
Alejandro Reyes <arcaela.reyes@gmail.com>
2023-05-08 13:56:31 -0400

    Jest Config

---
+++ jest.config.ts
@@ -0,0 +1,8 @@
+import { type Config } from "jest"
+
+export default {
+    preset: "ts-jest",
+    verbose: true,
+    // moduleFileExtensions: [...defaults.moduleFileExtensions, 'mjs', 'cjs'],
+    // "testEnvironment": "node"
+} as Config
